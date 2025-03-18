409af2a2dbdf63d12a117bdc94b11fddb2355208
Matias Sarapalo <sarapalo.matias@gmail.com>
2025-03-18 17:17:56 +0200

    Show recipe list/info

--- items.py
+++ items.py
@@ -4,3 +4,18 @@ def add_item(title, ingredients, recipe, classification, user_id):
     sql = """INSERT INTO items (title, ingredients, recipe, classification, user_id)
              VALUES (?, ?, ?, ?, ?)"""
     db.execute(sql, [title, ingredients, recipe, classification, user_id])
+
+def get_items():
+    sql = "SELECT id, title FROM items ORDER BY id DESC"
+    return db.query(sql)
+
+def get_item(item_id):
+    sql = """SELECT items.title,
+                    items.ingredients,
+                    items.recipe,
+                    items.classification,
+                    users.username
+             FROM items, users
+             WHERE items.user_id = users.id AND
+                   items.id = ?"""
+    return db.query(sql, [item_id])[0]
