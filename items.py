30814f2169aec646122b95d4ae3542825776e1e1
Matias Sarapalo <sarapalo.matias@gmail.com>
2025-03-18 19:03:17 +0200

    Added edit recipes feature

--- items.py
+++ items.py
@@ -10,12 +10,22 @@ def get_items():
     return db.query(sql)
 
 def get_item(item_id):
-    sql = """SELECT items.title,
+    sql = """SELECT items.id,
+                    items.title,
                     items.ingredients,
                     items.recipe,
                     items.classification,
+                    users.id user_id,
                     users.username
              FROM items, users
              WHERE items.user_id = users.id AND
                    items.id = ?"""
     return db.query(sql, [item_id])[0]
+
+def update_item(item_id, title, ingredients, recipe, classification):
+    sql = """UPDATE items SET title = ?,
+                              ingredients = ?,
+                              recipe = ?,
+                              classification = ?
+                          WHERE id = ?"""
+    db.execute(sql, [title, ingredients, recipe, classification, item_id])
