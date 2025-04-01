ea823178269cdca9188c157e10d7c959b01d20fd
Matias Sarapalo <sarapalo.matias@gmail.com>
2025-04-01 20:19:55 +0300

    Store classes in database

--- app.py
+++ app.py
@@ -47,7 +47,8 @@ def show_item(item_id):
 @app.route("/new_item")
 def new_item():
     require_login()
-    return render_template("new_item.html")
+    classes = items.get_all_classes()
+    return render_template("new_item.html", classes=classes)
 
 @app.route("/create_item", methods=["POST"])
 def create_item():
@@ -68,12 +69,10 @@ def create_item():
     user_id = session["user_id"]
 
     classes = []
-    cuisine = request.form["cuisine"]
-    if cuisine:
-        classes.append(("Keittiö", cuisine))
-    difficulty = request.form["difficulty"]
-    if difficulty:
-        classes.append(("Vaikeusaste", difficulty))
+    for entry in request.form.getlist("classes"):
+        if entry:
+            parts = entry.split(":")
+            classes.append((parts[0], parts[1]))
 
     items.add_item(title, ingredients, recipe, classification, user_id, classes)
 
---
+++ init.sql
@@ -0,0 +1,10 @@
+DELETE FROM classes;
+
+INSERT INTO classes (title, value) VALUES ('Keittiö', 'italialainen');
+INSERT INTO classes (title, value) VALUES ('Keittiö', 'ranskalainen');
+INSERT INTO classes (title, value) VALUES ('Keittiö', 'kiinalainen');
+INSERT INTO classes (title, value) VALUES ('Keittiö', 'amerikkalainen');
+
+INSERT INTO classes (title, value) VALUES ('Vaikeusaste', 'helppo');
+INSERT INTO classes (title, value) VALUES ('Vaikeusaste', 'haastava');
+INSERT INTO classes (title, value) VALUES ('Vaikeusaste', 'vaikea');
