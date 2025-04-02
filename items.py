51759b14f266858e44a8d0d04d57175d0d95904d
Matias Sarapalo <sarapalo.matias@gmail.com>
2025-04-02 11:40:46 +0300

    Added classes to edit page

--- items.py
+++ items.py
@@ -45,13 +45,20 @@ def get_item(item_id):
     result = db.query(sql, [item_id])
     return result[0] if result else None
 
-def update_item(item_id, title, ingredients, recipe, classification):
+def update_item(item_id, title, ingredients, recipe, classification, classes):
     sql = """UPDATE items SET title = ?,
                               ingredients = ?,
                               recipe = ?,
                               classification = ?
                           WHERE id = ?"""
     db.execute(sql, [title, ingredients, recipe, classification, item_id])
+
+    sql = "DELETE FROM item_classes WHERE item_id = ?"
+    db.execute(sql, [item_id])
+
+    sql = "INSERT INTO item_classes (item_id, title, value) VALUES (?, ?, ?)"
+    for title, value in classes:
+        db.execute(sql, [item_id, title, value])
 
 def remove_item(item_id):
     sql = "DELETE FROM items WHERE id = ?"
